576a26c6a0147c46125f98db9b1616f5021f0808
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-11-11 17:38:24 -0600

    Installed redux and added routing for home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import './App.css';
+import React from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+
+// import Header from '../Header/Header';
+// import Footer from '../Footer/Footer';
+// import WordlePage from '../WordlePage/WordlePage';
+// import CrosswordPage from '../CrosswordPage/CrosswordPage';
+// import ConnectionsPage from '../ConnectionsPage/ConnectionsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Resume Games</h1>
-        <div>
-          Wordle
-        </div>
-        <div>
-          Crossword
-        </div>
-        <div>
-          Connections
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <Header /> */}
+
+        <Switch>
+          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+          <Redirect exact from="/" to="/home" />
+
+          {/* <Route exact path="/wordle">
+            <WordlePage />
+          </Route>
+          
+          <Route exact path="/crossword">
+            <CrosswordPage />
+          </Route>
+          
+          <Route exact path="/connections">
+            <ConnectionsPage />
+          </Route> */}
+          
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
